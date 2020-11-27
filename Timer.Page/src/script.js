f116cd97bd09ea4569ec810f228dadfaf7da100f
ykropchik22 <40296340+ykropchik@users.noreply.github.com>
2020-11-27 14:50:58 +1000

    Added touchscreen support

--- Timer.Page/src/script.js
+++ Timer.Page/src/script.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', initialisation);
 var hours = 0;
 var minutes = 0;
 var seconds = 0;
+let lastTouch = null;
 
 function initialisation() {
     document.getElementById('info-icon-container').addEventListener('mouseup', function() {
@@ -23,6 +24,8 @@ function initialisation() {
         }, 500);
     });
 
+    document.getElementById('timer-container').addEventListener('touchstart', () => {lastTouch = event.changedTouches[0].clientY;});
+    document.getElementById('timer-container').addEventListener('touchmove', changeTimer);
     document.getElementById('timer-container').addEventListener('wheel', changeTimer);
 
     document.getElementById('start-btn').addEventListener('mouseup', function() {
@@ -50,8 +53,7 @@ function initialisation() {
                     document.getElementById('hours').innerHTML = hours < 10 ? '0' + hours : hours;
                     document.getElementById('minutes').innerHTML = minutes < 10 ? '0' + minutes : minutes;
                     document.getElementById('seconds').innerHTML = seconds < 10 ? '0' + seconds : seconds;
-
-                    console.log(new Date().getTime() - startTime >= timerSec * 1000);
+                    
                     if (new Date().getTime() - startTime >= timerSec * 1000) {
                         resolve();
                     }
@@ -72,33 +74,59 @@ function initialisation() {
 }
 
 const changeTimer = function(event) {
-    switch (event.target.id) {
-        case 'hours':
-            if (event.deltaY < 0) { 
-                if (++hours === 24) { hours = 0;}
-            } else {
-             if (--hours === -1) { hours = 23;}
-            }
-            
-            document.getElementById('hours').innerHTML = hours < 10 ? '0' + hours : hours;
-            break;
-         case 'minutes':
-             if (event.deltaY < 0) { 
-                 if (++minutes === 60) { minutes = 0;}
-             } else {
-                 if (--minutes === -1) { minutes = 59;}
-             }
-
-             document.getElementById('minutes').innerHTML = minutes < 10 ? '0' + minutes : minutes;
-             break;
-         case 'seconds':
-             if (event.deltaY < 0) { 
-                 if (++seconds === 60) { seconds = 0;}
-             } else {
-                 if (--seconds === -1) { seconds = 59;}
-             }
-
-             document.getElementById('seconds').innerHTML = seconds < 10 ? '0' + seconds : seconds;
-             break;
+    if (event.changedTouches[0]) {
+        switch (event.target.id) {
+            case 'hours':
+                changeHour(lastTouch - event.changedTouches[0].clientY);
+                break;
+             case 'minutes':
+                changeMinute(lastTouch - event.changedTouches[0].clientY);
+                break;
+             case 'seconds':
+                changeSeconds(lastTouch - event.changedTouches[0].clientY);
+                break;
+        }
+    } else {
+        switch (event.target.id) {
+            case 'hours':
+                changeHour(event.deltaY);
+                break;
+             case 'minutes':
+                changeMinute(event.deltaY);
+                break;
+             case 'seconds':
+                changeSeconds(event.deltaY);
+                break;
+        }
     }
+}
+
+function changeHour(delta) {
+    if (delta < 0) { 
+        if (++hours === 24) { hours = 0;}
+    } else {
+        if (--hours === -1) { hours = 23;}
+    }
+
+    document.getElementById('hours').innerHTML = hours < 10 ? '0' + hours : hours;
+}
+
+function changeMinute(delta) {
+    if (event.deltaY < 0) { 
+        if (++minutes === 60) { minutes = 0;}
+    } else {
+        if (--minutes === -1) { minutes = 59;}
+    }
+
+    document.getElementById('minutes').innerHTML = minutes < 10 ? '0' + minutes : minutes;
+}
+
+function changeSeconds(delta) { 
+    if (event.deltaY < 0) { 
+        if (++seconds === 60) { seconds = 0;}
+    } else {
+        if (--seconds === -1) { seconds = 59;}
+    }
+
+    document.getElementById('seconds').innerHTML = seconds < 10 ? '0' + seconds : seconds;
 }
